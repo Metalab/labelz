a903f7125b6ce9646857a32c8cb761d738e9e70b
Jascha Ehrenreich <jascha@jaeh.at>
2014-11-28 08:48:21 +0100

    added field population on document.ready

---
+++ js/labelz.js
@@ -0,0 +1,57 @@
+'use strict';
+
+// get current date
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0!
+var yyyy = today.getFullYear();
+
+if ( dd < 10 ) {
+	dd = '0' + dd;
+}
+
+if ( mm < 10 ) {
+	mm = '0' + mm;
+}
+today = yyyy+'-'+mm+'-'+dd;
+
+$(document).ready( function() {
+	$('.currentDate').val(today);
+
+	$('.displayDate').html(today);
+
+	$('#preview, #finish').hide(); //selects both html elements
+
+	//.on is also triggered for elements added after the dom finished loading
+	$("input").on('keyup', update);
+	$("input, select").on('change', update);
+
+	$('select').trigger('change'); //triggers the change event on page load
+	livesurr();
+});
+
+// surrender text
+function livesurr() {
+	$('.liveinput').each( function() {
+		var id = $(this).attr('id');
+		if ( id ) { //if we have no idea this function would error fatally
+			var livesurr = $(this).attr('id').replace('config', 'surr');
+
+			//toggle shown or hidden state of the element 
+			$('#'+livesurr).toggle();
+		}
+	});
+}
+
+function update() {
+	$('#preview, #finish').slideDown('slow');
+
+	var id = $(this).attr('id');
+
+	if ( id ) {
+		var liveinput = id.replace('config', 'display');
+		$('#' + liveinput).html( $(this).val() );
+	}
+
+	livesurr();
+}
