3e09591f984210244caf9aca7f380bcec7c0b512
fraggerfox <santhosh.raju@gmail.com>
2015-10-21 23:39:19 +0530

    labelz.css
    1. Adjusted the font size a bit
    2. Added some styling information to the new sections added for QR Code feature
    
    index.html
    
    1. Added two new sections for QR Code generation
    2. Cleaned up the HTML a bit
    
    labelz.js
    
    1. Added support for QR Code generation in the new sections

--- js/labelz.js
+++ js/labelz.js
@@ -6,6 +6,12 @@ var dd = today.getDate();
 var mm = today.getMonth() + 1; //January is 0!
 var yyyy = today.getFullYear();
 
+var hasQRCode = false;
+
+var qrcodePersonal = null;
+var qrcodeFreeStyle = null;
+var qrcode = null;
+
 if ( dd < 10 ) {
 	dd = '0' + dd;
 }
@@ -22,7 +28,8 @@ $(document).ready( function() {
 
 	$('#preview, #finish').hide(); //selects both html elements
 	$('#configFreeStyle, #outputFreeStyle').hide(); //selects both html elements
-
+	$('#outputPersonalWithQRCode, #outputFreeStyleWithQRCode').hide(); //selects both html elements
+    
 	$("#chooseType > li").on('click', chooseType);
 
 	//.on is also triggered for elements added after the dom finished loading
@@ -39,12 +46,14 @@ function livesurr() {
 		var id = $(this).attr('id');
 		if ( id ) { //if we have no idea this function would error fatally
 			var livesurr = $(this).attr('id').replace('config', 'surr');
-
+            
 			//toggle shown or hidden state of the element 
 			if ( $(this).val() == "" ) {
 				$('#'+livesurr).hide();
+                $('#'+livesurr+'WithQRCode').hide();
 			} else {
 				$('#'+livesurr).show();
+                $('#'+livesurr+'WithQRCode').show();                
 			}
 		}
 	});
@@ -60,8 +69,46 @@ function chooseType() {
 		//removing "selected" class from all choose "buttons"
 		$("#chooseType > li").removeClass("typeSelected");
 
-		var config = type.replace('choose', 'config');
-		var print = type.replace('choose', 'output');
+        if(type.indexOf('WithQRCode') == -1) {
+            hasQRCode = false;
+		    var config = type.replace('choose', 'config');
+		    var print = type.replace('choose', 'output');
+        } else {
+            hasQRCode = true;
+            var originalType = type.replace('WithQRCode', '');
+            var config = originalType.replace('choose', 'config');
+		    var print = type.replace('choose', 'output');
+
+            if(qrcodePersonal == null) {
+                qrcodePersonal = new QRCode("displayPersonalQRCode", {
+                    text: "Your text goes here...",
+                    width: 75,
+                    height: 75,
+                    colorDark : "#000000",
+                    colorLight : "#ffffff",
+                    correctLevel : QRCode.CorrectLevel.H
+                });
+            }
+
+            if(qrcodeFreeStyle == null) {
+                qrcodeFreeStyle = new QRCode("displayFreeStyleQRCode", {
+                    text: "Your text goes here...",
+                    width: 75,
+                    height: 75,
+                    colorDark : "#000000",
+                    colorLight : "#ffffff",
+                    correctLevel : QRCode.CorrectLevel.H
+                });
+            }
+
+            if(type.indexOf("Personal") > -1) {
+                qrcode = qrcodePersonal;
+            }
+
+            if(type.indexOf("FreeStyle") > -1) {
+                qrcode = qrcodeFreeStyle;
+            }
+        }
 
 		//add "selected" class to the used "button" and unhide chosen form and print view
 		$('#' + type).addClass("typeSelected");
@@ -77,9 +124,23 @@ function update() {
 	var id = $(this).attr('id');
 
 	if ( id ) {
-		var liveinput = id.replace('config', 'display');
-		$('#' + liveinput).html( $(this).val() );
-	}
+        var liveinput = id.replace('config', 'display');
+        
+		$('#' + liveinput + 'WithQRCode').html( $(this).val() );
+        $('#' + liveinput).html( $(this).val() );
+        
+        if ( hasQRCode ) {
+            if(liveinput.indexOf("Personal") > -1) {
+                var url = $('#outputPersonalWithQRCode #displayPersonalUrlWithQRCode').text();
+                console.log(url);
+                qrcode.makeCode(url);
+            }
 
+            if(liveinput.indexOf("FreeStyle") > -1) {
+                qrcode.makeCode($(this).val());
+            }
+        }
+	}
+    
 	livesurr();
 }
